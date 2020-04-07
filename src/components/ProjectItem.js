c32f6d48832886b1ad618c84e4c7dc8cc0284ed2
Bashir <b_egeh@hotmail.com>
2020-04-07 17:39:33 -0400

    flex box added to projects section

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default function ProjectItem (){
   return(
     <a href="#"><img src="http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg"
-            className="img2" width="30%" height="228px" alt="Underconstruction" /></a>
+            className="img2" max-width="100%" height="228px" alt="Underconstruction" /></a>
   )
 }
