62ca702f388995d1191c2c86928cc75d77874529
Bashir <b_egeh@hotmail.com>
2020-04-07 16:55:51 -0400

    create components folder and ProjectItem component

---
+++ src/components/ProjectItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function ProjectItem (){
+  return(
+    <a href="#"><img src="http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg"
+            className="img2" width="30%" height="228px" alt="Underconstruction" /></a>
+  )
+}
