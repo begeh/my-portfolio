cb211de826aec11b530b16d42c903df879bdc519
Bashir <b_egeh@hotmail.com>
2020-04-07 18:09:14 -0400

    remove hard corded links from projects section

--- src/App.js
+++ src/App.js
@@ -12,6 +12,30 @@ const projects = [
 {
   imageUrl: "http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg",
   linkUrl: "#"
+},
+{
+  imageUrl: "http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg",
+  linkUrl: "#"
+},
+{
+  imageUrl: "http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg",
+  linkUrl: "#"
+},
+{
+  imageUrl: "http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg",
+  linkUrl: "#"
+},
+{
+  imageUrl: "http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg",
+  linkUrl: "#"
+},
+{
+  imageUrl: "http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg",
+  linkUrl: "#"
+},
+{
+  imageUrl: "http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg",
+  linkUrl: "#"
 }
 ]
 function App() {
@@ -51,50 +75,25 @@ function App() {
               I'm a pharmacist based out of the Scarborough district of Toronto. I'm looking to diversify my skills and take on new challenges in the expanding field of computer programming. I aim to learn as much and participate in as many projects as I can in order to improve my coding skills. One day, I hope to pursue a career in video game development and contribute to a medium that has had such a profound influence on my life.
             </p>
         </div>
-          <h2 className="project_title"><a name="toprojects"></a>PROJECTS </h2>
+        <h2 className="project_title"><a name="toprojects"></a>PROJECTS </h2>
+        <div className="project">
+          {
+            projects.map((project, index)=>(
+              <ProjectItem imageUrl={project.imageUrl} linkUrl={project.linkUrl} />
+            ))
+          }   
+        </div>
 
-          <div className="project">
-            {
-              projects.map((project, index)=>(
-                <ProjectItem imageUrl={project.imageUrl} linkUrl={project.linkUrl} />
-              ))
-            }
-
-            <a href="https://codepen.io/BEgeh/full/PWJxmo/" target="_blank"><img src="https://i.imgur.com/SQdnJRq.png" 
-            className="img2" alt="Photo of Profile project" /></a>
-
-            <a href="#"><img src="http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg"
-            className="img2" alt="Underconstruction" /></a>
-              
-            <a href="#"><img src="http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg"
-            className="img2" alt="Underconstruction" /></a>
-                
-            <a href="#"><img src="http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg"
-            className="img2" alt="Underconstruction" /></a>      
-
-            <a href="#"><img src="http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg"
-            className="img2" alt="Underconstruction" /></a>
-              
-            <a href="#"><img src="http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg"
-            className="img2" alt="Underconstruction" /></a>
-                
-            <a href="#"><img src="http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg"
-            className="img2" alt="Underconstruction" /></a>   
-                
-            <a href="#"><img src="http://www.clipartkid.com/images/254/website-currently-under-construction-APcYla-clipart.jpg"
-            className="img2" alt="Underconstruction" /></a>   
-          </div>
-
-          <h1 className="contact-header"><a name="tocontact"></a>CONTACT ME</h1>
-          <h2>Email: b_egeh@hotmail.com</h2>
-          <div>
-            <a href="https://www.facebook.com/bashirabdi.egeh" target="_blank"><img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQDvg36emnVR8gQKmtDb-jQOl1fkDTpSjdOpt9oEpGh7TX_wILLXhz3xg" alt="Facebook Link" width="70px" align="left" className="spacing" /></a>
-            <h2 className="icon"> Facebook</h2>
-          </div>
-          <div>
-            <a href="https://ca.linkedin.com/in/bashir-egeh-776954129" target="_blank"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhUQ-8JWaklA2oYy8tMvU-IX0fEymeq4W2laB1xjvuWYVwIV_dKCl6wA" alt="LinkedIn Link" width="70px" align="left" className="spacing" /></a>
-            <h2 className="icon"> LinkedIn</h2>
-          </div>
+        <h1 className="contact-header"><a name="tocontact"></a>CONTACT ME</h1>
+        <h2>Email: b_egeh@hotmail.com</h2>
+        <div>
+          <a href="https://www.facebook.com/bashirabdi.egeh" target="_blank"><img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQDvg36emnVR8gQKmtDb-jQOl1fkDTpSjdOpt9oEpGh7TX_wILLXhz3xg" alt="Facebook Link" width="70px" align="left" className="spacing" /></a>
+          <h2 className="icon"> Facebook</h2>
+        </div>
+        <div>
+          <a href="https://ca.linkedin.com/in/bashir-egeh-776954129" target="_blank"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhUQ-8JWaklA2oYy8tMvU-IX0fEymeq4W2laB1xjvuWYVwIV_dKCl6wA" alt="LinkedIn Link" width="70px" align="left" className="spacing" /></a>
+          <h2 className="icon"> LinkedIn</h2>
+        </div>
       </body>
   );
 }
